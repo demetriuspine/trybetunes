15673b879d67f0835d4ac25ed50ad61e92d933cf
lucas <mr.pine97@gmail.com>
2021-09-14 15:35:04 -0300

    impl: switcher

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Switcher from './components/Switcher';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Switcher />
+      </BrowserRouter>
+    );
   }
 }
 
