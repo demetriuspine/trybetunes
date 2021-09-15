4eae6f9b8934e6a4bee408d7c4cce636a1eccb37
lucas <mr.pine97@gmail.com>
2021-09-15 12:59:25 -0300

    impl: Adicionando form no Search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,43 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+    };
+  }
+
+  handleChange = (event) => {
+    const { value, name } = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { search } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>search</p>
+        <form>
+          <label htmlFor="search-artist-input">
+            <input
+              data-testid="search-artist-input"
+              name="search"
+              type="text"
+              value={ search }
+              onChange={ this.handleChange }
+            />
+            <input
+              data-testid="search-artist-button"
+              type="button"
+              value="Pesquisar"
+              disabled={ search.length <= 1 }
+            />
+          </label>
+        </form>
       </div>
     );
   }
