531121ff7ae49a25edea67f7fc8183fc4f67422c
lucas <mr.pine97@gmail.com>
2021-09-15 19:46:01 -0300

    refact: soluçao mais elegante - wrapperType

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -2,29 +2,37 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class MusicCard extends Component {
+  artistDetails() {
+    const { music: { artistName, collectionName } } = this.props;
+    return (
+      <section>
+        <h2 data-testid="artist-name">{ artistName }</h2>
+        <h3 data-testid="album-name">{ collectionName }</h3>
+      </section>
+    );
+  }
+
+  songs() {
+    const { music: { trackName, previewUrl } } = this.props;
+    return (
+      <section>
+        <p>{trackName}</p>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          <code>audio</code>
+        </audio>
+      </section>
+    );
+  }
+
   render() {
-    const { music: {
-      trackName, previewUrl, kind, artistName, collectionName } } = this.props;
-    if (kind === undefined) {
-      return (
-        <section>
-          <h2 data-testid="artist-name">{ artistName }</h2>
-          <h3 data-testid="album-name">{ collectionName }</h3>
-        </section>
-      );
-    }
-    if (kind === 'song') {
-      return (
-        <section>
-          <p>{trackName}</p>
-          <audio data-testid="audio-component" src={ previewUrl } controls>
-            <track kind="captions" />
-            O seu navegador não suporta o elemento
-            <code>audio</code>
-          </audio>
-        </section>
-      );
-    }
+    const { music: { wrapperType } } = this.props;
+    return (
+      <main>
+        { wrapperType === 'collection' ? this.artistDetails() : this.songs() }
+      </main>
+    );
   }
 }
 
@@ -32,7 +40,7 @@ MusicCard.propTypes = {
   music: PropTypes.shape({
     trackName: PropTypes.string,
     previewUrl: PropTypes.string,
-    kind: PropTypes.string,
+    wrapperType: PropTypes.string,
     artistName: PropTypes.string,
     collectionName: PropTypes.string,
   }).isRequired,
