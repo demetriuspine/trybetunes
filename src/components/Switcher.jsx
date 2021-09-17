55498d7984edf644f356904a69c10f8d4a8ed2a4
lucas <mr.pine97@gmail.com>
2021-09-17 15:08:53 -0300

    refact: eliminaçao do BrowserRouter do componente Switcher - redundancia

--- src/components/Switcher.jsx
+++ src/components/Switcher.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Album from '../pages/Album';
 import Favorites from '../pages/Favorites';
 import Login from '../pages/Login';
@@ -11,17 +11,15 @@ import Search from '../pages/Search';
 export default class Switcher extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={ Login } />
-          <Route path="/album/:id" component={ Album } />
-          <Route path="/profile/edit" component={ ProfileEdit } />
-          <Route path="/search" component={ Search } />
-          <Route path="/favorites" component={ Favorites } />
-          <Route path="/profile" component={ Profile } />
-          <Route path="*" component={ NotFound } />
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
+        <Route path="/search" component={ Search } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
     );
   }
 }
